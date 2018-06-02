156727e88c82621edc85ff91d7e2e80e79db6ced
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-06-02 19:12:44 -0400

    init

---
+++ nutshell/01_basics/circularArea.c
@@ -0,0 +1,9 @@
+// circularArea.c: Calculate and print the areas of circles
+
+// Calculate the area of a circle
+// Param: r: the circle's radius
+// Return: the circle's area
+double circularArea(double r) {
+  const double PI = 3.1415926536;
+  return PI * r * r;
+}
