dc095a38e728f5edced0ea8e2fb800c0de89b7e5
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-11-24 12:37:04 -0800

    added printf

---
+++ coursera/progFund/02/printing/printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+  int a = 42;
+  int b = 7;
+
+  printf("Hello, World!\n");
+  printf("a\n");
+  printf("a: %d\n", a);
+  printf("b: %d\na + b = %d\n", b, a + b);
+  printf("Hello again!\n\n");
+  printf("My answer:\t%d\n", b);
+  
+  return 0;
+}
