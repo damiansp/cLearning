7ad42555e936a17ec9383ec18746075919ed47f8
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2020-05-21 22:02:03 -0700

    +realines, strscan

---
+++ prog/09_characterStrings/readlines.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// read a line of text from the command line
+void readLine(char buffer[]) {
+  char character;
+  int  i = 0;
+
+  do {
+    character = getchar();
+    buffer[i] = character;
+    ++i;
+  } while (character != '\n');
+  buffer[i - 1] = '\0';
+}
+
+
+int main() {
+  char line[81];
+
+  for (int i = 0; i < 3; ++i) {
+    readLine(line);
+    printf("%s\n\n", line);
+  }
+  return 0;
+}
